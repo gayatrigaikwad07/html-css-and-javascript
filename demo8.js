8ca6b88b01ee3d40efa7783ef354765b70f79ff1
gayatrigaikwad07 <154303689+gayatrigaikwad07@users.noreply.github.com>
2023-12-28 19:36:27 +0530

    Add files via upload

---
+++ demo8.js
@@ -0,0 +1,38 @@
+
+/*let fname ="Yash";
+console.log(fname);*/
+
+var interesetRate=0.3;
+interesetRate=9;
+console.log(interesetRate);
+
+let fname ="Yash";
+console.log(typeof(fname));
+
+let name ="True";
+console.log(typeof(fname));
+
+let na =null;
+console.log(typeof(na));
+
+let me =undefined;
+console.log(typeof(me));
+
+let selectedColours1 = []
+console.log(selectedColours1)
+let selectedColours2 = ['red','blue']
+console.log(selectedColours2);
+console.log(selectedColours2[1])
+
+
+//Function
+function greet(){
+    console.log("Hi");
+}
+greet();
+
+//Function
+function greet(name){
+    console.log("Hi" + name);
+}
+greet("Yog");
